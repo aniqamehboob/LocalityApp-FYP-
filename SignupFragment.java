88e71435db72569f90298f19e3d5e5e747c393fe
Aniqa Mehboob <aniqamehboob@users.noreply.github.com>
2016-10-11 20:34:32 +0500

    Add files via upload
    Adding files

---
+++ SignupFragment.java
@@ -0,0 +1,61 @@
+package com.aniqa_mehboob.loginandsignupactivity.Fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.aniqa_mehboob.loginandsignupactivity.R;
+
+
+public class SignupFragment extends Fragment implements CompoundButton.OnCheckedChangeListener
+
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.signup_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
+}
+
